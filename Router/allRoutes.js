f5b8e00317306a60d8dfd7551fe059e6d1e72f21
Guillaume0402 <168349909+Guillaume0402@users.noreply.github.com>
2025-03-11 06:52:38 +0100

    Ajout .htaccess pour configurer la page 404 a la place de wamp

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,8 +1,7 @@
 import Route from "./Route";
 
 //Définir ici vos routes
-export const allRoutes = [
-    new Route("/", "Accueil", "/QuaiAntiqueRestaurantFront/pages/home.html"),
+export const allRoutes = [    
     new Route("/", "Accueil", "pages/home.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
